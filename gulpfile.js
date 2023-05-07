6dee0b01f9aa0cfda9c187e88256b0a25793bbd6
Vladislav Fedorov <ma3za@list.ru>
2023-05-07 12:17:30 +0500

    successful initialization

--- gulpfile.js
+++ gulpfile.js
@@ -25,17 +25,7 @@ gulp.task("build-js", () => {
                         rules: [
                           {
                             test: /\.m?js$/,
-                            exclude: /(node_modules|bower_components)/,
-                            use: {
-                              loader: 'babel-loader',
-                              options: {
-                                presets: [['@babel/preset-env', {
-                                    debug: true,
-                                    corejs: 3,
-                                    useBuiltIns: "usage"
-                                }]]
-                              }
-                            }
+                            exclude: /(node_modules|bower_components)/
                           }
                         ]
                       }
