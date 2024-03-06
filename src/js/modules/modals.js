82e5c6375cb8a06ad6dcb8306887b1309bf7a5a4
Vlad Fedorov <79852880+Vlad19982602@users.noreply.github.com>
2024-03-06 12:28:09 +0500

    modal
    continue work with modal

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -56,7 +56,8 @@ const modals = () => {
     
     bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
     bindModal('.popup_calc_btns', '.popup_calc_profile', '.popup_calc_profile_close', false);
-    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+    bindModal('.t862__btn_next', '.popup_calc_end', '.popup_calc_end_close', false);
+    bindModal('.t862__btn_next', '.popup_stock', '.popup_calc_stock_close', false);
     // showModalByTime('.popup', 60000);
 };
 
