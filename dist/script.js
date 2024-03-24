0ef188ccafb75fc5a2ad36a36280d04436a97cb1
Vlad Fedorov <79852880+Vlad19982602@users.noreply.github.com>
2024-03-24 17:21:14 +0500

    clear version

--- dist/script.js
+++ dist/script.js
@@ -14326,10 +14326,13 @@ const modals = () => {
 
     bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
     bindModal('.phone_link', '.popup', '.popup .popup_close');
+    bindModal('.contact_us-btn', '.popup', '.popup .popup_close');
     
     bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
     bindModal('.popup_calc_btns', '.popup_calc_profile', '.popup_calc_profile_close', false);
     bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+    bindModal('.popup_calc_profile_button-5', '.popup_calc_four', '.popup_calc_end_close-four', false);
+    bindModal('.popup_calc_profile_button-2', '.popup_calc_five', '.popup_calc_end_close-five', false);
     // showModalByTime('.popup', 60000);
 };
 
@@ -14438,19 +14441,21 @@ const timeId = () => {
         if(block) {
             block.classList.add('t824__popup-container-visible', 'animate__animated', 'animate__fadeInUp');
         }
-    }, 5000)
+    }, 60000)
 
     if(close) {
         close.addEventListener('click', () => {
             windows.forEach(item => {
                 item.style.display = 'none';
             });
-    
-            // modal.style.display = "none";
-            // document.body.style.overflow = "";
-            // document.body.classList.remove('modal-open');
         });
     }
+
+    // setInterval(() => {
+    //     if(block.close) {
+    //         block.classList.toggle('t824__popup-container-visible', 'animate__animated', 'animate__fadeInUp');
+    //     }
+    // }, 1000)
 }
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (timeId);
